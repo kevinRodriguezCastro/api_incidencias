d07137f630335abf5d0b3db835987cdf5a8966c9
kevinRodriguezCastro <10208@cifpceuta.es>
2024-04-04 11:24:24 +0200

    Clases arregladas

--- src/main/java/api_incidencias/api_incidencias/Repositorios/RepositorioIncidencia.java
+++ src/main/java/api_incidencias/api_incidencias/Repositorios/RepositorioIncidencia.java
@@ -1,9 +1,18 @@
 package api_incidencias.api_incidencias.Repositorios;
 
 import api_incidencias.api_incidencias.Entidades.Incidencia;
+import api_incidencias.api_incidencias.Entidades.IncidenciaReabierta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RepositorioIncidencia extends JpaRepository<Incidencia, Long> {
+    @Query("SELECT i FROM Incidencia i WHERE i.usuarioCliente.idUsuario = ?1")
+    List<Incidencia> findByCliente(Long idCliente);
+
+    @Query("SELECT i FROM Incidencia i WHERE i.usuarioTecnico.idUsuario = ?1")
+    List<Incidencia> findByTecnico(Long idTecnico);
 }
--- src/main/java/api_incidencias/api_incidencias/Repositorios/RepositorioIncidenciaReabierta.java
+++ src/main/java/api_incidencias/api_incidencias/Repositorios/RepositorioIncidenciaReabierta.java
@@ -3,6 +3,11 @@ package api_incidencias.api_incidencias.Repositorios;
 import api_incidencias.api_incidencias.Entidades.Comentario;
 import api_incidencias.api_incidencias.Entidades.IncidenciaReabierta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RepositorioIncidenciaReabierta extends JpaRepository<IncidenciaReabierta, String> {
+    @Query("SELECT i FROM IncidenciaReabierta i WHERE i.incidenciaPrincipal.idIncidencia = ?1")
+    List<IncidenciaReabierta> findByIncidencia(Long idIncidencia);
 }
