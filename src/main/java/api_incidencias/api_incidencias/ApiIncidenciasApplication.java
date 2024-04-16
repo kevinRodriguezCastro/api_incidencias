97ac01d48a85caee4753698e0b8e8f1004ec34f8
miludCifp <10395@cifpceuta.es>
2024-04-16 10:53:17 +0200

    Cambios de ayer

--- src/main/java/api_incidencias/api_incidencias/ApiIncidenciasApplication.java
+++ src/main/java/api_incidencias/api_incidencias/ApiIncidenciasApplication.java
@@ -2,10 +2,11 @@ package api_incidencias.api_incidencias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("api_incidencias.api_incidencias.Servicios.Seguridad")
 public class ApiIncidenciasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiIncidenciasApplication.class, args);
 	}
